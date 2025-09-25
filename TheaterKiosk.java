4790b3f105b8e7e13805e049e7c500c7d0d8a877
A-Wa11 <wallinan@mail.uc.edu>
2025-09-25 13:19:11 -0400

    Add files via upload

---
+++ TheaterKiosk.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        if (in.hasNextInt()) {
+            int age = in.nextInt();
+            if (age >= 21) {
+                System.out.println("You get a wrist band.");
+            }
+        }
+    }
+}
